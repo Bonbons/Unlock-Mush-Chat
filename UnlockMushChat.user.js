30254e67c4304fdc4d81cbfffb968d219eb4bd3c
Bonbons <tapuna@hotmail.com>
2016-06-20 11:57:18 -1000

    Update UnlockMushChat.user.js

--- UnlockMushChat.user.js
+++ UnlockMushChat.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Unlock Mush Chat
 // @namespace    http://mush.vg/
-// @version      0.8
-// @description  Unlock Mush Chat 
+// @version      0.9
+// @description  Unlock Mush Chat
 // @author       BonbonsDealer
 // @downloadURL https://raw.githubusercontent.com/Bonbons/Unlock-Mush-Chat/master/UnlockMushChat.user.js
 // @match        http://mush.vg/*
@@ -12,8 +12,11 @@
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant		GM_addStyle
-// @grant		GM_getResourceText 
+// @grant		GM_getResourceText
 // ==/UserScript==
+
+/*jshint multistr: true */
+
 /*
  @require        http://code.jquery.com/ui/1.11.4/jquery-ui.js
  @resource       jqueryUiCss http://code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css
@@ -50,13 +53,13 @@ unsafeWindow.Main.chooseSkill = function (jq) {
 }*/
 console.log("Main ",unsafeWindow.Main);
 
-var unlockingG = GM_getValue(window.location.host+'_unlockingG',false);	
+var unlockingG = GM_getValue(window.location.host+'_unlockingG',false);
 if (unlockingG||unlockingG=='true') {
     GM_registerMenuCommand("Stop Unlock General Chat", function() {stopUnlockGeneralChat();});
     unlockGeneralChat();
 } else {
     GM_registerMenuCommand("Unlock General Chat", function() {unlockGeneralChat();});
-}		
+}
 
 function unlockGeneralChat() {
     GM_setValue(window.location.host+'_unlockingG',true);
@@ -66,6 +69,7 @@ function unlockGeneralChat() {
     $('#localtab').after(
 '<li id="walltab" data-tab="5" onclick="Main.selChat(5)" ondblclick="return false;" class="tab taboff"><img src="/img/icons/ui/wall.png" onmouseover="Main.showTip(this,\'<div class=\\\'tiptop\\\' ><div class=\\\'tipbottom\\\'><div class=\\\'tipbg\\\'><div class=\\\'tipcontent\\\'><h1>Discussion</h1><p>Le canal de Discussion est indispensable pour s\\\'organiser avec l\\\'équipage. </p><p>Pour participer vous devez posséder un <img src=&quot;/img/icons/ui/talkie.png&quot;/> <strong>talkie-walkie</strong>.</p></div></div></div></div>\')" onmouseout=" Main.hideTip(); "><div class="hide tab_not_read" style="display: none;"><span class="cdNbNotRead">0</span></div></li>'
         );
+    $("table.treereply tr.not_read.cdRepl").css("display", "table-row");
 }
 
 function stopUnlockGeneralChat() {
@@ -74,7 +78,7 @@ function stopUnlockGeneralChat() {
     timer=window.setTimeout(reloading,1000);
 }
 
-var unlockingM = GM_getValue(window.location.host+'_unlockingM',false);	
+var unlockingM = GM_getValue(window.location.host+'_unlockingM',false);
 if (unlockingM||unlockingM=='true') {
     GM_registerMenuCommand("Stop Unlock Mush Chat", function() {stopUnlockMushChat();});
     unlockMushChat();
@@ -95,35 +99,42 @@ function stopUnlockMushChat() {
     timer=window.setTimeout(reloading,1000);
 }
 
-function addCss(cssString) { 
+function addCss(cssString) {
     try {
-        var head = document.getElementsByTagName('head')[0]; 
-        //return unless head; 
+        var head = document.getElementsByTagName('head')[0];
+        //return unless head;
         if (head) {
-            var newCss = document.createElement('style'); 
-            newCss.type = "text/css"; 
-            newCss.innerHTML = cssString; 
-            head.appendChild(newCss); 
+            var newCss = document.createElement('style');
+            newCss.type = "text/css";
+            newCss.innerHTML = cssString;
+            head.appendChild(newCss);
 			newCss=null;
         }
 		head=null;
     } catch (e) {
         console.error("Error in addCss",e);
     }
-};
+}
 
 var myWindow;
 function openWin(text) {
     myWindow = window.open("", "", "width=400 ,height=200");
-	console.log("myWindow ",myWindow); 
+	console.log("myWindow ",myWindow);
 	if (myWindow) {
-		myWindow.document.write("<html><head><script language='javascript'>function changeparent(){try{console.log('test');window.opener.external.comeback(this);}catch(e){var back = confirm(e);if(back) {_child.close();}}}</script></head><body>");
+		var closetimer;
+		var closefunc = function () {myWindow.close(); myWindow=null;};
+		myWindow.document.write("<html><head><script language='javascript'>function changeparent(){try{console.log('test');window.opener.external.comeback(this);}catch(e){var back = confirm(e);if(back) {_child.close();	myWindow=null;}}}</script></head><body>");
 		myWindow.document.write("<form><input type=button onclick='javascript:changeparent()' value='Main'></form>");
 		myWindow.document.write("<p id='textinfo' onclick='javascript:this.opener.focus()' >"+text+"</p>");
 		myWindow.document.write("</body></html>");
 		myWindow.document.title = text;
+		try {
+			closetimer=myWindow.setTimeout(closefunc,15*1000);
+		} catch (e) {
+			console.error("closetimer error:",e);
+		}
 	} else {
-		console.log("POPUP is blocked",myWindow); 
+		console.log("POPUP is blocked",myWindow);
 	}
 }
 window.external.comeback = function(_child) {
@@ -134,9 +145,9 @@ window.external.comeback = function(_child) {
 	} else {
 		_child.focus();
 	}
-}
+};
 
-console.log("window.location.host ",window.location.host);  
+console.log("window.location.host ",window.location.host);
 var checking = GM_getValue(window.location.host+'_checking',false);
 var joining = GM_getValue(window.location.host+'_joining',false);
 var unreading = GM_getValue(window.location.host+'_unreading',false);
@@ -144,7 +155,6 @@ var timer;
 var mCoinSound = new Audio("https://dl.dropbox.com/u/7079101/coin.mp3");
 var mClickSound = new Audio("http://soundbible.com/grab.php?id=1705&type=mp3");
 var persList = {
-	
 	Andie: {
 		img:"andie_graham",
 		name:"Andie Graham"
@@ -217,10 +227,8 @@ var persList = {
 		img:"terrence_archer",
 		name:"Terrence Archer"
 		}
-	
 };
 
-		
 if (checking||checking=='true') {
     GM_registerMenuCommand("Stop Check complete crew", function() {stopCheckCompleteCrew();});
     checkCompleteCrew();
@@ -242,11 +250,12 @@ function checkCompleteCrew() {
 }
 
 function reloading() {
-    console.log("Reloading",checking,joining); 
+    console.log("Reloading",checking,joining);
     if (!myWindow) {
-        console.log("myWindow was never opened",checking,joining); 
+        console.log("myWindow was never opened",checking,joining);
     } else {
         myWindow.close();
+		myWindow=null;
     }
     mClickSound.play();
     location.reload();
@@ -254,12 +263,12 @@ function reloading() {
 
 var persChoosen = GM_getValue(window.location.host+'_persChoosen',persList.Chao);
 if (joining||joining=='true') {
-    var _people=$('ul[class="people"]');    
+    var _people=$('ul[class="people"]');
     //GM_registerMenuCommand("Stop Join as Paola", function() {stopJoinAs();});
     GM_registerMenuCommand("Stop Join as "+persChoosen.name, function() {stopJoinAs();});
-	if (_people.length>0) {	
+	if (_people.length>0) {
 		_people=null;
-		console.log("You already join a party!!!"); 
+		console.log("You already join a party!!!");
 	} else {
 		_people=null;
 		joinAs(persChoosen);
@@ -276,7 +285,7 @@ function stopJoinAs() {
 }
 
 
-function beginJoinAs() { 
+function beginJoinAs() {
 	$("body").append ( '\
 	<div id="dialog-form" title="Choose character" class="ui-dialog ui-widget ui-widget-content ui-corner-all ui-draggable ui-resizable" tabindex="-1" role="dialog" aria-labelledby="ui-id-1" style="outline: 0px; z-index: 1004; position: absolute; height: auto; width: auto; top: 10%; left: 30%; display: block;">\
 	<div><button id="close_character" class="ui-dialog-titlebar-close ui-corner-all" role="button"><span class="ui-icon ui-icon-closethick">close</span></button></div>\
@@ -289,63 +298,61 @@ function beginJoinAs() {
 	</form>\
 	</div>\                                                                  \
 	' );
-	$('#character').css("color","rgb(34, 75, 165)")
+	$('#character').css("color","rgb(34, 75, 165)");
 	$.each(persList, function(name, person) {
-			console.log("#character",name,person);  
+			console.log("#character",name,person);
 			$('#character').append( $('<option></option>').val(name).html(person.name) );
 			if (GM_getValue(window.location.host+'_persChoosen',persList.Chao).img==person.img) {
 				$('#character').val(name);
 			}
-		}); 
+		});
 	$('#bt_character').click(function() {
 		var character = $( "#character" );
 		persChoosen=persList[character.val()];
-		console.log(persChoosen.name+" is the character choosen...",persChoosen);  
+		console.log(persChoosen.name+" is the character choosen...",persChoosen);
 		GM_setValue(window.location.host+'_persChoosen',persChoosen);
 		joinAs(persChoosen);
 	});
 	$('#close_character').click(function() {
 		$('#dialog-form').empty();
 		$('#dialog-form').remove();
-	});	
+	});
 }
 
 function joinAs(someone) {
     GM_setValue(window.location.host+'_joining',true);
     try {
-        var _people=$('table[class="choosehero2"]');    
-        if (_people.length>0) {	
-			
+        var _people=$('table[class="choosehero2"]');
+        if (_people.length>0) {
 			var _pers=$('img[src*="img/art/char/'+someone.img+'.jpg"]');
-			
-			console.log("Join As "+someone.name,_pers);    
+			console.log("Join As "+someone.name,_pers);
 			if (_pers.length>0) {
 				var _charDiv=$('div[class*="charDiv"][onclick*="'+someone.name+'"]');
 				openWin(someone.name+" is available!!!");
-				console.log(someone.name+" is available!!!",_charDiv);  
+				console.log(someone.name+" is available!!!",_charDiv);
 				document.title = someone.name+" is available!!!";
 				if (_charDiv.length>0) {
 					_charDiv.click();
 					mCoinSound.play();
 					if (!myWindow) {
-						console.log("myWindow was never opened",joining); 
+						console.log("myWindow was never opened",joining);
 					} else {
 						myWindow.focus();
 					}
 					var _Button=$('div[class*="cdChooseButton"]');
 					if (_Button.length>0) {
-						console.log("_Button ",_Button);  
+						console.log("_Button ",_Button);
 						_Button.click();
 					}
+				    _Button=null;
 				}
 				window.clearTimeout(timer);
 				_people=null;
 				_pers=null;
 				_charDiv=null;
-				_Button=null;
 				timer=window.setTimeout(reloading,30*1000);
 			} else {
-				console.log(someone.name+" isn't available...",joining);  
+				console.log(someone.name+" isn't available...",joining);
 				document.title = someone.name+" isn't available...";
 				window.clearTimeout(timer);
 				_people=null;
@@ -354,7 +361,7 @@ function joinAs(someone) {
 			}
 		}
     } catch (e) {
-        console.error("Error checkCompleteCrew",e);        
+        console.error("Error checkCompleteCrew",e);
     }
 }
 
@@ -363,7 +370,7 @@ if (unreading||unreading=='true') {
     checkUnread();
 } else {
     GM_registerMenuCommand("Check Unread", function() {checkUnread();});
-} 
+}
 
 function stopCheckUnread() {
     GM_setValue(window.location.host+'_unreading',false);
@@ -376,7 +383,6 @@ function checkUnread() {
 	timer=window.setTimeout(searchAjax,1000);
 }
 
-	
 function myAjax(page, params, cbError, cbSuccess) {
 	try {
 
@@ -402,28 +408,29 @@ function myAjax(page, params, cbError, cbSuccess) {
 		console.error("ERROR in myAjax: " + err.stack);
 		return false;
 	}
-};
+}
 
 
 function refreshing(_id, _text ) {
     $('#'+_id+'').html(_text);
 }
 function refresh(data) {
-    console.log("refresh(data)"); 
+    console.log("refresh(data)");
 	refreshing('topinfo_bar', $('#topinfo_bar', data).html() );
 	refreshing('chat_col', $('#chat_col', data).html() );
 	refreshing('cdInventory', $('#cdInventory', data).html() );
 	if ($('#research_module', data).length>0) {
-		refreshing('research_module', $('#research_module', data).html() );	
+		refreshing('research_module', $('#research_module', data).html() );
 	}
 	if ($('#cdModuleContent', data).length>0) {
-		refreshing('cdModuleContent', $('#cdModuleContent', data).html() );	
+		refreshing('cdModuleContent', $('#cdModuleContent', data).html() );
 	}
 	refreshing('char_col', $('#char_col', data).html() );
+    $("table.treereply tr.not_read.cdRepl").css("display", "table-row");
 }
 
-function searchAjax() {	
-	
+function searchAjax() {
+
 	function onError() {
 		console.log("Unable to use ajax");
 		timer=window.setTimeout(searchAjax,20*1000);
@@ -434,7 +441,7 @@ function searchAjax() {
 		var d2 = new Date();
 		var n = d.toTimeString();
 		var cycletime = $('p[class="cycletime"]').text().trim();
-		if ($('ul[class="people"]', data).length>0) {	
+		if ($('ul[class="people"]', data).length>0) {
 			var last_nb_mush = GM_getValue(window.location.host+'_nb_mush',0);
 			var nb_mush = $('img[src*="p_mush.png"]', data).length;
 			console.log($('p[class="cycletime"]').text());
@@ -446,7 +453,7 @@ function searchAjax() {
 				mCoinSound.play();
 				var back = confirm(cycletime+" ("+n+"): Mush number altered!!! ("+last_nb_mush+" to "+nb_mush+")");
 				if(back) {
-					mClickSound.play();	
+					mClickSound.play();
 				}
 				d2 = new Date();
 				if (((d2-d)/1000)>60) {
@@ -456,21 +463,22 @@ function searchAjax() {
 				}
 			}
 		}
-		
+
 		unreading=GM_getValue(window.location.host+'_unreading',false);
 		if (unreading||unreading=='true') {
 			var tot_unread=0;
 			var lastNbUnread = GM_getValue(window.location.host+'_lastNbUnread',0);
 			$('img[class*="recent"]', data).each(function(_i, _e) {
-				tot_unread+=1;            
+				tot_unread+=1;
 			});
 			GM_setValue(window.location.host+'_lastNbUnread',tot_unread);
-			console.log("count:",tot_unread); 
-			if (tot_unread==0) {
-				console.log("No unread message...",checking);  
+			console.log("count:",tot_unread);
+			if (tot_unread===0) {
+				console.log("No unread message...",checking);
 				document.title = "No unread message...";
 				if (myWindow) {
 					myWindow.close();
+					myWindow=null;
 				}
 				window.clearTimeout(timer);
 				timer=window.setTimeout(searchAjax,30*1000);
@@ -484,11 +492,11 @@ function searchAjax() {
 				} else {
 					if (!myWindow) {
 						openWin(message);
-						console.log(message,tot_unread);  
+						console.log(message,tot_unread);
 						document.title = "Unread Message!!!";
 						mCoinSound.play();
 						if (!myWindow) {
-							console.log("myWindow was never opened",checking); 
+							console.log("myWindow was never opened",checking);
 						} else {
 								myWindow.focus();
 						}
@@ -503,36 +511,34 @@ function searchAjax() {
 		checking = GM_getValue(window.location.host+'_checking',false);
 		if (checking||checking=='true') {
 			try {
-				var _people=$('ul[class="people"]', data);    
-				if (_people.length>0) {	
-					
+				var _people=$('ul[class="people"]', data);
+				if (_people.length>0) {
 					var _cryo=$('img[src*="p_cryo.png"]', data);
-					
-					//console.log("Check complete crew",_cryo);    
+					//console.log("Check complete crew",_cryo);
 					if (_cryo.length>0) {
-						console.log("Still crew in cryo...",checking);  
+						console.log("Still crew in cryo...",checking);
 						document.title = "Still crew in cryo...";
 						window.clearTimeout(timer);
 						_people=null;
 						_cryo=null;
 						timer=window.setTimeout(searchAjax,30*1000);
 					} else {
-						var message = cycletime+" ("+n+"): Complete Crew!!!"
+						var message_complete = cycletime+" ("+n+"): Complete Crew!!!";
 						if (!myWindow) {
-							openWin(message);
-							console.log(message,checking);  
+							openWin(message_complete);
+							console.log(message_complete,checking);
 							document.title = "Complete Crew!!!";
 							mCoinSound.play();
 							if (!myWindow) {
-								console.log("myWindow was never opened",checking); 
+								console.log("myWindow was never opened",checking);
 							} else {
 								myWindow.focus();
-								myWindow.alert(message);
+								myWindow.alert(message_complete);
 							}
 						} else {
-							myWindow.document.getElementById('textinfo').innerHTML = message;
+							myWindow.document.getElementById('textinfo').innerHTML = message_complete;
 							myWindow.focus();
-							myWindow.alert(message);
+							myWindow.alert(message_complete);
 						}
 						window.clearTimeout(timer);
 						_people=null;
@@ -541,11 +547,11 @@ function searchAjax() {
 					}
 				}
 			} catch (e) {
-				console.error("Error checkCompleteCrew",e);        
+				console.error("Error checkCompleteCrew",e);
 			}
 		}
 	}
-	
+
 	try {
 		var params = {};
 		myAjax('#', params, onError, onSuccess);
@@ -555,4 +561,4 @@ function searchAjax() {
 		timer=window.setTimeout(searchAjax,20*1000);
 		return false;
 	}
-};
+}
